da9051166763840e2cd3da6a3ce644697a738cf2
Shubham Sharma <shubhamsharma21505@gmail.com>
2025-06-24 22:52:18 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,46 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const progressBars = document.querySelectorAll('.progress');
+
+  progressBars.forEach(bar => {
+    const value = bar.getAttribute('data-skill');
+    bar.style.width = `${value}%`;
+  });
+});
+
+const texts = document.querySelectorAll('.text');
+let index = 0;
+
+function showText() {
+  // Remove active class from current text
+  texts[index].classList.remove('active');
+
+  // Move to the next index
+  index = (index + 1) % texts.length;
+
+  // Add active class to the next text
+  texts[index].classList.add('active');
+}
+
+// Show the next text every 1.5 seconds
+setInterval(showText, 1250);
+
+const form = document.getElementById("contactForm");
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const formData = {
+    name: form.name.value,
+    email: form.email.value,
+    message: form.message.value
+  };
+
+  const res = await fetch("http://localhost:3001/send-email", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(formData)
+  });
+
+  const data = await res.text();
+  document.getElementById("responseMsg").innerText = data;
+});
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { Resend } = require("resend");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const resend = new Resend("re_F66P4oaZ_JmEmdfehDwgCixiSHabTM1cQ");
+
+app.post("/send-email", async (req, res) => {
+  const { name, email, message } = req.body;
+
+  try {
+    await resend.emails.send({
+      from: "Shubham Sharma <onboarding@resend.dev>",
+      to: "shubhamsharma21505@gmail.com",
+      subject: "New Contact Form Message " + new Date().toLocaleTimeString(),
+      text: `Name: ${name}\nEmail: ${email}\nMessage:\n${message}`,
+    });
+
+    res.send("✅ Message sent successfully!");
+  } catch (error) {
+    console.error("❌ Error sending email:", error);
+    res.status(500).send("❌ Failed to send message.");
+  }
+});
+
+app.listen(3001, () => {
+  console.log("🚀 Server running on http://localhost:3001");
+});
